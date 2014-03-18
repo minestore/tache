b5b362b4c1e5b3b89ed09d539d0bbdc3b17196ce
Raphael <raphael@raphaelcosta.net>
2014-03-17 22:44:42 -0300

    force encoding

--- lib/tache/template.rb
+++ lib/tache/template.rb
@@ -3,37 +3,37 @@ class Tache::Template
     @source = source
     @tags = options[:tags]
   end
-  
+
   def compile
     tokenize
     self
   end
-  
+
   def render(context, indent = '')
     render_tokens(tokenize, context, indent)
   end
-  
+
   def compiled?
     !!@tokens
   end
-  
+
   private
-  
+
   def tokenize
     @tokens ||= Tache::Parser.new.parse(@source, @tags)
   end
 
   def render_tokens(tokens, context, indent = '')
     buffer = ''
-    
+
     tokens.each do |token|
       type = token[0]
       token_value = token[1]
-      
+
       case type
       when '#'
         value = context[token_value]
-                
+
         if value == true
           buffer << render_tokens(token[2], context)
         elsif value.respond_to?(:call)
@@ -63,13 +63,13 @@ class Tache::Template
       end
     end
 
-    buffer
+    buffer.force_encoding('utf-8')
   end
-  
+
   def resolve(context, value, indent, newline, escape)
     original = value
     value = value.to_tache_value if value.respond_to?(:to_tache_value)
-    
+
     if value.is_a?(Tache::Template)
       # FIXME: We're forcing a new context here when rendering the template,
       # however we will already be in a new context when this resolve call is
@@ -93,7 +93,7 @@ class Tache::Template
       escape ? context.escape(value) : value
     end
   end
-  
+
   def interpolate(source, context, tags = nil)
     if source.is_a?(Tache::Template)
       # TODO: Refactor.
@@ -102,7 +102,7 @@ class Tache::Template
       self.class.new(source.to_s, tags: tags)
     end.render(context.dup)
   end
-  
+
   # Based on JavaScript implementation
   def falsy?(value)
     !value || value.respond_to?(:empty?) && value.empty? ||
